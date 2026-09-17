6e491861f62992257308679bb81849873ffa9020
Zac-Smucker-Bryan <zacary.bryan@gmail.com>
2026-09-16 20:14:05 -0500

    Create Home.tsx and import

---
+++ src/Home.tsx
@@ -0,0 +1,10 @@
+export const Home = () => {
+    return (
+        <div>
+            <h1>Home</h1>
+            <button className="btn btn-soft btn-lg">
+                Set up a Game
+            </button>
+        </div>
+    );
+};
